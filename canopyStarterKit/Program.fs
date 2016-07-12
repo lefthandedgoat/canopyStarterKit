352a7b9f3af2f99afea10e6860786a67742de380
Robin Munn <robin_munn@sil.org>
2016-07-12 13:22:26 +0700

    Parse args first
    This allows --help to be caught right away, instead of opening up a
    Chrome window and *then* catching --help.

--- canopyStarterKit/Program.fs
+++ canopyStarterKit/Program.fs
@@ -6,11 +6,12 @@ open Common
 
 [<EntryPoint>]
 let main argv =
+  let args = Args.parse argv
+
   configuration.chromeDir <- executingDir()
   reporter <- new LiveHtmlReporter(Chrome, configuration.chromeDir) :> IReporter
   reporter.setEnvironment "canopy test page"
 
-  let args = Args.parse argv
   start args.Browser
 
   Tests.register args.Tag args.TestType
